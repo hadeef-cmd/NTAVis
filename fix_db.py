520198ecd0967608c466922a8bac98b0a91ff749
Hadif Shah <hadifshah177@gmail.com>
2025-11-04 01:06:17 +0800

    Delete fix_db.py

--- fix_db.py
+++
@@ -1,8 +0,0 @@
-# fix_db.py
-import sqlite3
-conn = sqlite3.connect("packets.db")
-cursor = conn.cursor()
-cursor.execute("DROP TABLE IF EXISTS packets")
-conn.commit()
-conn.close()
-print("Table dropped. Run your capture script to recreate it.")
